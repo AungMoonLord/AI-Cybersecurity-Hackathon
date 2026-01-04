5a6b80bdedc1df624170a2add2c3c1ea7707ab80
Waris Sripatoomrak <167295008+waris-sri@users.noreply.github.com>
2026-01-05 01:04:04 +0700

    Delete [Demo] Sample Queries/splitQueries.js

--- [Demo] Sample Queries/splitQueries.js
+++
@@ -1,8 +0,0 @@
-const fs = require("node:fs");
-fs.readFile("queries-logs-100A.txt", "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data.split("\n").map((line) => ({ query: line.trim() })));
-});
